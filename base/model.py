6ed97ab6af9426cddb5fa1ba1cfc0eb7ca7b8bf4
usmankhan <uka3304@gmail.com>
2023-07-23 15:33:07 +0500

    updated

--- base/model.py
+++ base/model.py
@@ -4,3 +4,6 @@ class BaseModel(models.Model):
     uid=models.UUIDField( primary_key=True, editable=False, default=uuid.uuid4)
     created_at=models.DateTimeField (auto_now_add= True)
     updated_at=models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        abstract = True
--- products/models.py
+++ products/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from base.model import *
 # Create your models here.
+class Category(BaseModel):
+    category_name=models.CharField(max_length=100, )
+    category_image=models.ImageField( upload_to='categories' )
+
+class Products(BaseModel):
+    product_name=models.CharField( max_length=100)
+    category=models.ForeignKey(Category, on_delete=models.CASCADE,related_name="products" )
+    price=models.IntegerField()
+    produtct_description= models.TextField()
+
+class productsImages(BaseModel):
+    product=models.ForeignKey(Products, on_delete=models.CASCADE,related_name="procuts_images")
+    category_image=models.ImageField( upload_to='categories' )
