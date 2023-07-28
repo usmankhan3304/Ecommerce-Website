89c2a5378d3d520b62627927b9b63e29d2d4ebdf
usmankhan <uka3304@gmail.com>
2023-07-28 07:45:45 +0500

    updated

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from accounts import views
 
 urlpatterns = [
-    path('login_page/',views.login_page, name="login_page")
+    path('login_page/',views.login_page, name="login_page"),
+    path("register_page",views.register_page, name="register_page"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -6,3 +6,5 @@ from django.shortcuts import render
 # Create your views here.
 def login_page(request):
     return render(request,"accounts/login_page.html")
+def register_page(request):
+    return render(request,"accounts/register.html")
