9c057f14b1780d4982f50ad0bfeeffe4120b4c66
usmankhan <uka3304@gmail.com>
2023-08-03 01:55:06 +0500

    Ecommerece website

--- accounts/models.py
+++ accounts/models.py
@@ -6,15 +6,39 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save
 import uuid
 from base.emails import send_account_verification_email
+from products.models import *
 # Create your models here.
 # Create your models here.
 class UserProfile(BaseModel):
     user=models.OneToOneField(User, on_delete=models.CASCADE, related_name="user_profile")
     is_email_verified=models.BooleanField(default=False)
     email_token=models.CharField( max_length=100,null=True,blank=True)
+    
     profile_iamge=models.ImageField( upload_to='profile' )
+
+    def cart_count(self):
+        return CartItem.objects.filter(cart__is_paid=False,cart__user=self.user).count()
+
     def __str__(self):
         return self.user.first_name
+
+class Cart(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE,related_name='carts')
+    is_paid=models.BooleanField(default=False)
+
+
+class CartItem(BaseModel):
+    cart = models.ForeignKey(Cart,on_delete=models.CASCADE,related_name="cart_items")
+    product=models.ForeignKey(Products, related_name="products", on_delete=models.SET_NULL, null=True)
+    color_varient= models.ForeignKey(ColorVarient,  on_delete=models.SET_NULL,null=True)
+    size_varient= models.ForeignKey(SizeVarient,  on_delete=models.SET_NULL,null=True)
+    def __str__(self):
+        return self.product.product_name
+
+
+
+
+
 
 @receiver(post_save, sender= User)
 def send_email_token(sender,instance,created, **kwargs):
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
+from products.views import *
 from accounts import views
 
 urlpatterns = [
     path('login_page/',views.login_page, name="login_page"),
     path("register_page/",views.register_page, name="register_page"),
-    path('activate/<email_token>/',views.activate,name="activate")
+    path('activate/<email_token>/',views.activate,name="activate"),
+    path("add-to-cart/<uid>/",add_to_cart , name="add_to_cart")
 ]
